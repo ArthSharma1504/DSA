47f99292b05305b8b3f863d82dcc1ef78c042924
Arth Sharma <arthsharma1504@gmail.com>
2025-02-11 11:18:23 +0000

    palindrome done

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,3 +1,30 @@
+import java.util.Arrays;
+
 public class StringPalindrome {
-    
+    public static void main(String[] args) {
+        String[] str = {"racecar", "madam", "rose", "car", "A man, a plan, a canal: Panama"}; // Added more test cases
+        isPalindrome(str);
+        // System.out.println(Arrays.toString(str));
+    }
+
+    public static void isPalindrome(String[] words){
+        for(int i = 0; i<words.length; i++){
+            boolean checkPalindrome = checkPalindrome(words[i]);
+            System.out.println(words[i]+ ":" + checkPalindrome);
+        }
+    }
+
+    public static boolean checkPalindrome(String word) {
+        String processedWord = word.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        int left = 0; 
+        int right = processedWord.length() - 1;
+        while(left<right){
+            if(processedWord.charAt(left) != processedWord.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
